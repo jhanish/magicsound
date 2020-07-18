9551820a33245bd170ee3934fe019ba91653487a
Joe Hanish <joe.hanish@gmail.com>
2020-07-18 14:30:38 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="magicsound", # Replace with your own username
+    version="0.0.1",
+    author="Joe Hanish",
+    author_email="joe.hanish@gmail.com",
+    description="Super simple sound player",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/magicsound",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+s
